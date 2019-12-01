abcdba68ae29c9529ee611babfe16e86b007ce3f
Alejo Z <zitoalejo@gmail.com>
2019-12-01 17:24:55 -0300

    Api gateway local testing + removed ssl from api

--- NetCore3_api.ApiGateway/Startup.cs
+++ NetCore3_api.ApiGateway/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using System;
@@ -25,7 +26,7 @@ namespace NetCore3_api.ApiGateway
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public async void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
