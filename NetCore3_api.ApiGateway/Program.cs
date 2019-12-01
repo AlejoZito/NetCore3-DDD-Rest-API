eb3f066d34b0e58f307c77fa065d70e42f760bfa
Alejo Z <zitoalejo@gmail.com>
2019-12-01 17:10:27 -0300

    Api gateway
    Agrego un api gateway muy sencillo, que permite un load balancing también muy básico. Queda pendiente configurarlo al hostearlo

---
+++ NetCore3_api.ApiGateway/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace NetCore3_api.ApiGateway
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config
+                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddJsonFile("ocelot.json")
+                    .AddEnvironmentVariables();
+            })
+            .UseStartup<Startup>();
+    }
+}
